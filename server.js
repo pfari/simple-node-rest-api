adb05d978bcbe38a1d181a6c82df9c444e7e02fb
Paolo Farinella <pfari@Paolos-MacBook-Pro-2.local>
2017-06-15 14:53:23 +0200

    Fixed log4js interfering with mocha

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-//let log4js = require( "log4js" );
-//log4js.configure( "./config/log4js.json" );
-let logger = console;//log4js.getLogger("file-appender");
+let log4js = require( "log4js" );
+let logger = console;
 let express = require('express');
+let morgan = require('morgan');
 let app = express();
 let mongoose = require('mongoose');
-let morgan = require('morgan');
 let bodyParser = require('body-parser');
 let port = 8080;
 let book = require('./app/routes/book');
@@ -14,24 +13,25 @@ let options = {
                 replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } }
               };
 
+//Replace log4js with console when it is testing or will interfere with Mocha output
+if(config.util.getEnv('NODE_ENV') !== 'test') {
+  log4js.configure( "./config/log4js.json" );
+  logger = log4js.getLogger("file-appender");
+  app.use(morgan('combined'));
+}
+
 //db connection
 mongoose.connect(config.DBHost, options);
 let db = mongoose.connection;
-db.on('connected', console.log.bind(console, 'Connection DB established.'));
-db.on('error', console.error.bind(console, 'Connection DB error:'));
-db.on('disconnected', console.error.bind(console, 'connection error:'));
+db.on('connected', logger.info.bind(logger, 'Connection DB established.'));
+db.on('error', logger.error.bind(logger, 'Connection DB error:'));
+db.on('disconnected', logger.error.bind(logger, 'connection error:'));
 process.on('SIGINT', function() {
   db.close(function () {
-    console.log('Mongoose default connection disconnected through app termination');
+    logger.info('Mongoose default connection disconnected through app termination');
     process.exit(0);
   });
 });
-
-//don't show the log when it is test
-if(config.util.getEnv('NODE_ENV') !== 'test') {
-    //use morgan to log at command line
-    app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
-}
 
 //parse application/json and look for raw text
 app.use(bodyParser.json());
